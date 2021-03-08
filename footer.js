ed55d03742acb3223669258303d27f585820fb76
MuzaffarMohammed-Tuple <79186924+MuzaffarMohammed-Tuple@users.noreply.github.com>
2021-03-08 22:44:48 +0530

    MFORCE text changes

--- footer.js
+++ footer.js
@@ -73,7 +73,7 @@
    
        <div class="container">
          <div class="copyright">
-           &copy; Copyright <strong><span>MForce,INC</span></strong>. All Rights Reserved
+           &copy; Copyright <strong><span>MFORCE,INC</span></strong>. All Rights Reserved
          </div>
          <div class="credits">
            Designed by <a href="https://appseonit.com/">Appseonit Technologies Private Limited</a>
